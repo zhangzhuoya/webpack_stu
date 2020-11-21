c8d47579abf228e91f9fad99bd30616f36437c98
zhangzhuo <1114976368@qq.com>
2020-11-21 14:11:06 +0800

    模块化兼容学习

---
+++ 1.webpak的核心功能/1-3.模块化兼容性处理/src/commonjsa.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     a:1,
+//     b:2,
+//     c:3,
+//     // d
+// }
+// var d = 4;
+exports={
+    d:7
+
+}
+console.log("this is commonjss")
---
+++ 1.webpak的核心功能/1-3.模块化兼容性处理/src/index.js
@@ -0,0 +1,6 @@
+//使用commonjs导入
+// var obj = require("./commonjsa")
+// 使用es6导入
+import * as obj from "./commonjsa"
+console.log(obj);
+console.log("this is a index.js")
